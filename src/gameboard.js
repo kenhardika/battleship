80d459d0c31668338e2ab4784d251474a7b50499
kenhardika <kenzamahardika@gmail.com>
2022-08-31 12:22:19 +0700

    update gameboard(), receiveAttack() checkAllLocation() checkAttackMissed()

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,69 +1,93 @@
 import ships from "./ships.js";
 
 const gameboard = ()=> {
+    let allShip = [];
+    let totalHealth = 0;
+    let allLocation = [];
+    let attackMissed = [];
+
+    function refreshAllLocation(){
+        let newLocation = [];
+        allShip.forEach((ship)=>{
+            newLocation = newLocation.concat(ship.location());
+        });
+        allLocation = newLocation;
+    }
+    function attackMissedCounter(coor){
+        attackMissed.push(coor);
+    }
 
     console.log('gameboard is on');
-    let shipCoordinate = [];
-    let objShip = {
-        mainShip: [],
-        midShip: [],
-        smallShip: []
-    };
     return {
-        placement: (coor, ships)=>{ // dont need coor since coordinate should be inside the ship()
+        placement: (ships)=>{ // dont need coor since coordinate should be inside the ship()
             // make sure the coordinate is valid, which means empty and one block away from another ship
-            // place the ships on the coordinate      
-            shipCoordinate.push(coor.split(" "));
-            if (ships.length() === 5){
-                objShip.mainShip.push(ships);
-                objShip.mainShip.push(coor.split(" ")) // coordinate better put inside the ships
-                // objShip.mainShip = Object.assign(objShip.mainShip.ships, ships);
-                console.log(objShip); debugger
-            }
-            if (ships.length() === 3){
-                objShip.midShip.push(ships);
-                objShip.midShip.push(coor.split(" "))
-                // objShip.midShip = Object.assign(objShip.midShip.ships, ships);
-                console.log(objShip); debugger
-            }
-            if (ships.length() === 2){
-                objShip.smallShip.push(ships);
-                objShip.smallShip.push(coor.split(" "))
-                // objShip.smallShip = Object.assign(objShip.smallShip.ships, ships);
-                console.log(objShip); debugger
-            }
-            else{
-                return
-            }
+            // place the ships on the coordinate     
+            allShip.push(ships);
+            allLocation = allLocation.concat(ships.location());
             // marks the coordinate with ships' marks
             return
         },
-        receiveAttack: ()=>{
-            // check if its hit the ship
-            // if its hits the ship, toggle ship.hit()
+        receiveAttack: (coor)=>{
+            if(allLocation.includes(coor) === false){
+                console.log('Attack missed')
+                attackMissedCounter(coor);
+                return 
+            } 
+            else {
+                console.log('Attack Hit!')
+                allShip.forEach((ship)=>{
+                    ship.hit(coor);
+                });
+                // refresh the allLocation Array so you cannot hit twice on the same coordinate
+                refreshAllLocation()
+            } 
             // toggle checkAllShip() to make sure if its not endgame
             // if not marks the coordinate with missedAttack()
-            return
-        },
-        missedAttack: ()=>{
-            // mark the missed attack from opponnent
-            // disable the exact gameboard coordinate so you cannot receive attack on it again
-            return
+            //return
         },
         checkAllShip: ()=>{
             // check the healthbar of each ships with ship.healthbar()
+            allShip.forEach((ship)=>{
+                ship.location(); 
+                totalHealth = totalHealth + ship.healthBar();
+            });
+            console.log(totalHealth);
             // if all the healthbar is 0 then the game is ended
-            return
+            return totalHealth 
+        },
+        checkAllLocation: ()=>{
+            refreshAllLocation();
+            console.log(allLocation);
+            return allLocation
+        },
+        checkAttackMissed: ()=>{
+            console.log(attackMissed);
+            return attackMissed
         }
+
     }
 }
 
 const playa = gameboard();
+const bigShipCoor = '6B 7B 8B 9B 10B';
+const midShipCoor = '4B 4C 4D';
+const midShip = ships(midShipCoor);
+const bigShip = ships(bigShipCoor);
+
+playa.placement(bigShip);
+playa.placement(midShip);
+playa.receiveAttack("7B");debugger
+playa.receiveAttack("7B");debugger
+playa.receiveAttack("4D");debugger
+playa.receiveAttack("3B");debugger
+playa.checkAllLocation();
+playa.checkAttackMissed();
+playa.checkAllShip(); 
 // revise this
-playa.placement('1A 2A 3A', ships(3));
-playa.placement('3B 4B', ships(2));
-playa.placement('6B 7B 8B 9B 10B', ships(5));
-playa.placement('6E 7E 8E 9E 10E', ships(5));
+// playa.placement('1A 2A 3A', ships(3));
+// playa.placement('3B 4B', ships(2));
+// playa.placement('6B 7B 8B 9B 10B', ships(5));
+// playa.placement('6E 7E 8E 9E 10E', ships(5));
 
 
 export {gameboard}
--- src/ships.js
+++ src/ships.js
@@ -9,8 +9,8 @@ const ships = (loc) =>{ // length will be from size of the ship
         hit: (loc)=>{
             //get the attck hit location
             if (coord.includes(loc) === false){
-                console.log("attack missed")
-                return 
+                //console.log("attack missed")
+                return
             }
             else if(coord.includes(loc) === true){
                 coord = coord.filter( (val)=>{
@@ -35,37 +35,37 @@ const ships = (loc) =>{ // length will be from size of the ship
             }
         }, 
         healthBar : ()=> {
-            console.log("this ship health: " + healthBar);
+            //console.log("this ship health: " + healthBar);
             return healthBar
         },
         location: ()=>{
-            console.log(coord)
+            //console.log(coord)
             return coord
         }
     }
 }
 
-const bigShip = ships("3A 4A 5A 6A 7A");
-const midShip2 = ships("12A 12B 12C");
-const smallShip = ships("4B");
+// const bigShip = ships("3A 4A 5A 6A 7A");
+// const midShip2 = ships("12A 12B 12C");
+// const smallShip = ships("4B");
 
-bigShip.location();
-bigShip.healthBar();
-bigShip.hit("3A");
-bigShip.location();
-bigShip.healthBar();
-bigShip.hit("8A");
-bigShip.location();
-bigShip.healthBar();
-bigShip.hit("4A");
-bigShip.hit("5A");
-bigShip.hit("6A");
-bigShip.location();
-bigShip.healthBar();
-bigShip.isSunk();
-bigShip.hit("7A");
-bigShip.location();
-bigShip.healthBar();
-bigShip.isSunk();
+// bigShip.location();
+// bigShip.healthBar();
+// bigShip.hit("3A");
+// bigShip.location();
+// bigShip.healthBar();
+// bigShip.hit("8A");
+// bigShip.location();
+// bigShip.healthBar();
+// bigShip.hit("4A");
+// bigShip.hit("5A");
+// bigShip.hit("6A");
+// bigShip.location();
+// bigShip.healthBar();
+// bigShip.isSunk();
+// bigShip.hit("7A");
+// bigShip.location();
+// bigShip.healthBar();
+// bigShip.isSunk();
 
 export default ships
