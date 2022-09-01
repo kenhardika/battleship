ce1ee5d9068d32463cde7e82fd04455f64719230
kenhardika <kenzamahardika@gmail.com>
2022-09-01 16:37:12 +0700

    update psuedo logic, step what to do

--- src/player.js
+++ src/player.js
@@ -1,7 +1,20 @@
 import { gameboard } from "./gameboard";
 
 
+// player. What does player do.
+// player start the game by choosing who youre playing with
+// player choose is it gonna be vs AI or vs Human
+// at this point, if you choose AI. AI will automatically place their ships randomly on gameboard.
+// player choose the ships placement accross the gameboard.
+// ship's placement is based on one rule that there is always one empty block between one and another placed ships
+// player have a choice to randomly place the ships by clicking the random button. 
+// player placement order is.. first you place one big ship (5 in length), then two mid ship (3 in length), then three small ship (2 in length) 
+// after all ships are placed, game initiate to start attacking by choosing the opponents's gameboard. This phase you could hit opponents's ship.
+// after you attack opponent's ship, game automatically change to opponent's turn. This time Opponent's will initiate attack player's gameboard randomly. 
+// Game repeating the previous step until one of the player/AI ships are fully cleaned (all destroyed)
+// game ending if one of the player/AI total healthbar (ships) are = 0. 
+
 const player = gameboard();
-const human = gameboard();
+// const human = gameboard();
 const AI = gameboard();
 
